76e7c6d558f2054cc38bd7b842055364a3238ce2
LAPTOP-C8ML6OKD\Shree Ganesh <changelabanc20@gmail.com>
2024-03-19 11:22:01 +0530

    Vedio 52 complete

--- models/Note.js
+++ models/Note.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 // import mongoose from "mongoose";
 // const { Schema } = mongoose;
 
 const NoteSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
   title: {
     type: String,
     required: true,
